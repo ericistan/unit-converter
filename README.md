0b462d89c8db1a09929e15370a4c629f7cbfa4fb
Eric <63270620+ericistan@users.noreply.github.com>
2025-06-15 19:49:28 +0800

    update code for deployment

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # unit-converter
-
+Credits to Scrimba https://scrimba.com/ for providing the Solo project.
--- index.js
+++ index.js
@@ -3,3 +3,20 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+let convertBtn = document.getElementById("convert-button")
+let inputEl = document.getElementById("input-el")
+let convertLength = document.getElementById("convert-length")
+let convertArea = document.getElementById("convert-area")
+let convertVolume = document.getElementById("convert-volume")
+let convertMass = document.getElementById("convert-mass")
+
+
+convertBtn.addEventListener("click", function(){
+    let baseValue = inputEl.value
+    convertLength.textContent = `${baseValue} meters = ${(baseValue * 3.281).toFixed(2)} feet | ${baseValue} feet = ${(baseValue / 3.281).toFixed(2)} meters`
+    convertArea.textContent = `${baseValue} square meters = ${(baseValue * 	10.7639).toFixed(2)} square feet | ${baseValue} square feet = ${(baseValue / 10.7639).toFixed(2)} square meters`
+    convertVolume.textContent =`${baseValue} liters = ${(baseValue * 0.264).toFixed(2)} gallons | ${baseValue} gallons = ${(baseValue / 0.264).toFixed(2)} liters` 
+    convertMass.textContent =`${baseValue} kilos = ${(baseValue * 2.204).toFixed(2)} pounds | ${baseValue} pounds = ${(baseValue / 2.204).toFixed(2)} kilos` 
+
+})
